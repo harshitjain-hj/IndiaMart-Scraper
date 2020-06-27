858f005fb2a4ac8451b66004f9a330359ab5be0f
edoc99 <harshitjainf3@gmail.com>
2020-06-27 14:10:18 +0530

    1st commit

---
+++ README.md
@@ -0,0 +1,18 @@
+IndiaMart scraper
+===================================
+
+Scrapes the <b>supplier details (like Company name, Address, City, Phone no etc)</b> from the given link and generates .csv file for it.
+
+## Packages/Tools used -
+* [Selenium](https://pypi.org/project/selenium/)
+* [ChromeDriver - WebDriver](https://chromedriver.chromium.org/) 
+
+## How to use -
+1. Install the required packages and tools.
+2. Change the ChromeDriver path accordingly on line 8 in [indiamart_scrape](indiamart_scrape.py) python file.
+3. Change the url with the one required on line 10 in [indiamart_scrape](indiamart_scrape.py) python file.
+4. Run the python file.
+
+### Screenshot -
+  
+  <img src="screenshot.png" width="100%">
---
+++ indiamart_scrape.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+PATH = "C:\Program Files (x86)\chromedriver.exe";
+driver = webdriver.Chrome(PATH)
+driver.get("https://dir.indiamart.com/chennai/fresh-vegetables-all.html")
+try:
+    content = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "lay-lft"))
+    )
+
+    suppliers = content.find_elements_by_class_name("lst_cl")
+    # for supplier in suppliers:
+    company = [supplier.find_element_by_class_name("gcnm").text for supplier in suppliers]
+    address = [supplier.find_element_by_class_name("clg").text for supplier in suppliers]
+    precise_add = [supplier.find_element_by_class_name("cty-t").get_attribute("innerText") for supplier in suppliers]
+    phone_no = [supplier.find_element_by_css_selector(".bo").get_attribute("innerText") for supplier in suppliers]
+    items_list = []
+    for supplier in suppliers:
+        # items = supplier.find_elements_by_class_name("cp5")
+        item = [item.find_element_by_class_name("gpnm").text for item in supplier.find_elements_by_class_name("cp5")]
+        items_list.append(item)
+
+finally:
+    driver.quit()
+
+supplier_det = pd.DataFrame({
+    'Company': company,
+    'Address': address,
+    'Precise_add': precise_add,
+    'Phone_no': phone_no,
+    'Items': items_list
+})
+
+print(supplier_det)
+supplier_det.to_csv("sup_det.csv")
+
